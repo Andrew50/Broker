376ba1308aba3c17d7f3b703f07be14e88a5f7ef
owens <owensaltzman@gmail.com>
2023-10-21 21:11:51 -0600

    d

--- frontend/app.py
+++ frontend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, send_from_directory
 import random
-
+numbers= [1,2,3,4,5,6];
 app = Flask(__name__)
 
 # Path for our main Svelte page
@@ -15,7 +15,7 @@ def home(path):
 
 @app.route("/rand")
 def hello():
-    return str(random.randint(0, 100))
+    return str(numbers)
 
 if __name__ == "__main__":
     app.run(debug=True)
