e2cf6541e2fe655d969611bf540dc6f49a1072f0
billingsandrewjohnlaptop <billingsandrewjohn@gmail.com>
2023-10-21 21:37:59 -0400

    fn

---
+++ frontend/client/src/home/+home.svelte
@@ -0,0 +1,22 @@
+<script>
+	let count = 0;
+
+	function increment() {
+		count += 1;
+	}
+
+</script>
+
+
+<form action="" method="POST">
+    <p><input type="text" name="usr" size="10" /></p>
+    <p><input type="text" name="password" size="10" /></p>
+    <p><input type="submit" value="Send Request" /></p>
+</form>
+
+
+<button on:click={increment}>
+  
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
