a06415f5592f9407bbddf6da15234cc88a66883f
billingsandrewjohnlaptop <billingsandrewjohn@gmail.com>
2023-10-23 06:54:42 -0400

    z

--- frontend/src/routes/match/+server.js
+++ frontend/src/routes/match/+server.js
@@ -1,15 +1,18 @@
 
 
-export const GET = ({ request }) => {
+export const GET = async ({ request }) => {
     const authHeader = request.headers.get('Authorization')
 
 
-    if (!authHeader !== 'Myauthheader') {
-        return new response(JSON.stringify({ message: 'Invalid credentials' }), {
+    if (authHeader !== 'Myauthheader') {
+        return new Response(JSON.stringify({ message: 'Invalid credentials' }), {
             status: 401
         })
     }
+    const limit = Number(url.serachParams.gete('limit') ?? '10')
+    const skip = Number(url.serachParams.gete('skip') ?? '0')
 
-    const res = await fetch('https://')
-    return new Response(JSON.stringify({ message: "Hello" }), {status:200})
+    const res = await fetch('http://127.0.0.1:5000/posts?limit=${limit}&skip=${skip}')
+    const data = await res.json()
+    return new Response(JSON.stringify(data), { status: 200 })
 }
