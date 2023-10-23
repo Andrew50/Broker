3ef02985fda3f6cf6c869a2619e69eeb1e779248
billingsandrewjohnlaptop <billingsandrewjohn@gmail.com>
2023-10-22 21:51:29 -0400

    d

--- frontend/src/routes/match/+server.js
+++ frontend/src/routes/match/+server.js
@@ -1,6 +1,15 @@
 
 
-export const GET = () => {
+export const GET = ({ request }) => {
+    const authHeader = request.headers.get('Authorization')
 
+
+    if (!authHeader !== 'Myauthheader') {
+        return new response(JSON.stringify({ message: 'Invalid credentials' }), {
+            status: 401
+        })
+    }
+
+    const res = await fetch('https://')
     return new Response(JSON.stringify({ message: "Hello" }), {status:200})
 }
