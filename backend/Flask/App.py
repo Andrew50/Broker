a2885c12f72722017842ec1c9a349733138a28e4
owens <owensaltzman@gmail.com>
2023-10-22 22:15:12 -0600

    d

--- backend/Flask/App.py
+++ backend/Flask/App.py
@@ -2,10 +2,13 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api/data')
+@app.route('/api/data', methods=['GET'])
 def get_data():
-    data = {'message': 'Hello from Flask API!'}
-    return jsonify(data)
+    # Sample data to be returned for GET request
+    message = {
+        'message': 'This is a GET request'
+    }
+    return jsonify(message)
 
 if __name__ == '__main__':
     app.run()
