1915c51cd7136363c188833dbe4646b7f4d99d8c
owens <owensaltzman@gmail.com>
2023-10-22 19:53:30 -0600

    ds

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,22 +1,9 @@
 
 
 
-<div class ="index">
-    <body>
-        <h1>Input Form</h1>
-        <form>
-            <label for="ticker">Ticker:</label>
-            <input type="text" id="ticker" name="ticker" placeholder="Enter Ticker" required><br><br>
-
-            <label for="datetime">Datetime:</label>
-            <input type="datetime-local" id="datetime" name="datetime" required><br><br>
-
-            <label for="timeframe">Timeframe:</label>
-            <input type="text" id="timeframe" name="timeframe" placeholder="Enter Timeframe" required><br><br>
-
-            <input type="submit" value="Submit">
-        </form>
-    </body>
+<div class="index">
+    <h2>Welcome</h2>
+    <a href="/match">Match</a>>
 </div>
 
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { sveltekit } from '@sveltejs/kit/vite';
 
-// Docs: https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [svelte()],
-    server: {
-        proxy: {
-            '/api': 'http://localhost:5000'
-        }
-    }
-})
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit()]
+};
+
+export default config;
