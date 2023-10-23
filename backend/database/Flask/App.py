db725499e42cd961619ce12c1502446363a4ab02
billingsandrewjohnlaptop <billingsandrewjohn@gmail.com>
2023-10-22 21:37:58 -0400

    e

--- backend/database/Flask/App.py
+++ backend/database/Flask/App.py
@@ -2,10 +2,10 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api/data', methods=['GET'])
+@app.route('/api/data')
 def get_data():
-    data = {'message': 'Hello from Flask!'}
+    data = {'message': 'Hello from Flask API!'}
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
---
+++ frontend/src/routes/match/+page.svelte
@@ -0,0 +1,23 @@
+
+
+<div class ="index">
+    <body>
+        <h1>Match</h1>
+        <form>
+            <label for="ticker">Ticker:</label>
+            <input type="text" id="ticker" name="ticker" placeholder="Enter Ticker" required><br><br>
+
+            <label for="datetime">Datetime:</label>
+            <input type="datetime-local" id="datetime" name="datetime" required><br><br>
+
+            <label for="timeframe">Timeframe:</label>
+            <input type="text" id="timeframe" name="timeframe" placeholder="Enter Timeframe" required><br><br>
+
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+</div>
+
+
+
+<style></style>
