92f564dea7aa26f77e4bb425b93fb15e6251171b
billingsandrewjohnlaptop <billingsandrewjohn@gmail.com>
2023-10-25 20:26:23 -0400

    d

--- backend/database/Data.py
+++ backend/database/Data.py
@@ -4,12 +4,22 @@ db = mysql.connector.connect(
     host = "localhost",
     user="root",
     passwd="7+WCy76_2$%g",
+    database='broker'
 )
 
 c = db.cursor()
-c.execute('SELECT * FROM 
-#mycursor.execute("CREATE DATABASE testdatabase")
 
+
+
+
+def init():
+    df = c.execute('SELECT * FROM dfs')
+    print(df)
+    
+
+
+if __name__ == '__main__':
+    init()
 
 
 
