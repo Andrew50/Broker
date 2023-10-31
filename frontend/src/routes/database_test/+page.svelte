210855180b0c07f976730a1f10959cbb80dfdca1
billingsandrewjohnlaptop <billingsandrewjohn@gmail.com>
2023-10-31 13:37:30 -0400

    d

---
+++ frontend/src/routes/database_test/+page.svelte
@@ -0,0 +1,109 @@
+<!-- <h1>Candlestick Chart</h1> -->
+<Chart {...options}>
+    <CandlestickSeries
+        data={data}
+        reactive={true}
+        upColor="rgba(0,255, 0, 1)"
+        downColor="rgba(255, 0, 0, 1)"
+        borderDownColor="rgba(255, 144, 0, 1)"
+        borderUpColor="rgba(255, 144, 0, 1)"
+        wickDownColor="rgba(255, 144, 0, 1)"
+        wickUpColor="rgba(255, 144, 0, 1)"
+    />
+</Chart>
+<form on:submit={fetchData}>
+     <input type="text" id="ticker" bind:value ={ticker} name="ticker" placeholder="Enter Ticker" required>
+     <input type="text" id="tf" bind:value ={tf} name="ticker" placeholder="Enter TF" required>
+      <input type="text" id="dt" bind:value ={dt} name="ticker" placeholder="Enter Date Time">
+     <input type="submit" value="FETCH">
+</form>
+
+<script>
+
+    var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+    // Output the width and height
+    console.log("Window Width: " + windowWidth + " pixels");
+    console.log("Window Height: " + windowHeight + " pixels");
+
+    import {onMount} from 'svelte';
+    import {ColorType, CrosshairMode} from 'lightweight-charts';
+    import {Chart, CandlestickSeries} from 'svelte-lightweight-charts';
+    let ticker = "";
+    let tf = "";
+    let dt = "";
+    var data = [
+    { time: 1635528600000, open: 100, high: 110, low: 90, close: 105 },
+    { time: 1635528660000, open: 105, high: 115, low: 95, close: 100 },
+    // Add more candlestick data points for each minute
+    ];
+
+    
+
+    async function fetchData() {
+        const url = `http://127.0.0.1:5000/api/get2?ticker=${ticker}&tf=${tf}&dt=${dt}`;
+
+        const response = await fetch(url, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+
+        const responseData = await response.json();
+        data = responseData;
+        data.forEach(function(dataPoint) {
+            // Parse the date-time string and convert it to milliseconds
+            var dateTimeInMilliseconds = new Date(dataPoint.time).getTime();
+
+            // Update the 'time' property with milliseconds
+            dataPoint.time = dateTimeInMilliseconds;
+        });
+        CandlestickSeries.setData(data);
+        console.log('Unpacked Response:', data);
+    }
+
+    const options = {
+        width: windowWidth - 300,
+        height: windowHeight,
+        layout: {
+            background: {
+                type: ColorType.Solid,
+                color: '#000000',
+            },
+            textColor: 'rgba(255, 255, 255, 0.9)',
+        },
+        grid: {
+            vertLines: {
+                color: 'rgba(197, 203, 206, 0.5)',
+            },
+            horzLines: {
+                color: 'rgba(197, 203, 206, 0.5)',
+            },
+        },
+        crosshair: {
+            mode: CrosshairMode.Normal,
+        },
+        rightPriceScale: {
+            borderColor: 'rgba(197, 203, 206, 0.8)',
+        },
+        timeScale: {
+            borderColor: 'rgba(197, 203, 206, 0.8)',
+            tickMarkFormatter: (time, tickMarkType, locale) => {
+                const date = new Date(time);
+                const options = {
+                  year: 'numeric',
+                  month: 'short',
+                  day: 'numeric',
+                  hour: '2-digit',
+                  minute: '2-digit',
+                };
+                return date.toLocaleDateString(locale, options);
+              },
+        },
+    }
+    
+    
+
+</script>
--- frontend/src/routes/test2/+page.svelte
+++ frontend/src/routes/test2/+page.svelte
@@ -1,4 +1,3 @@
-
 <script>
     export let data = 'None';
     export let taskId = null;
@@ -6,8 +5,11 @@
     export let taskResult = null;
 
     async function startTask() {
-        const url = "http://127.0.0.1:5000/api/enqueue";
-        const requestData = { value: "someValue" }; // Include the necessary data
+        const url = "http://127.0.0.1:5000/api/add";
+        const requestData = {
+            num1: 10,
+            num2: 20
+        }; // JSON object with the required format
         const response = await fetch(url, {
             method: 'POST',
             headers: {
@@ -15,10 +17,9 @@
             },
             body: JSON.stringify(requestData) // Send the data as JSON
         });
-
         if (response.ok) {
             const responseData = await response.json();
-            taskId = responseData.task_id; // Assuming the backend sends a task_id
+            taskId = responseData.id; // Assuming the backend sends a task_id
             taskStatus = 'Pending';
             console.log('Task ID:', taskId);
         } else {
@@ -26,70 +27,14 @@
             data = 'Failed to start task';
         }
     }
-
-    async function checkTaskStatus() {
-        if (!taskId) {
-            alert('No task has been started yet!');
-            return;
-        }
-
-        const url = `http://127.0.0.1:5000/result/${taskId}`;
-        const response = await fetch(url);
-
-        if (response.ok) {
-            const responseData = await response.json();
-            taskStatus = responseData.status;
-            if (taskStatus === 'finished') {
-                taskResult = responseData.result;
-            }
-            console.log('Task Status:', taskStatus);
-            console.log('Task Result:', taskResult);
-        } else {
-            console.error('Request failed:', response.statusText);
-        }
-    }
+    
 </script>
 
 <form on:submit|preventDefault={startTask}>
     <input type="submit" value="Start Async Task">
 </form>
 
-<button on:click={checkTaskStatus}>Check Task Status</button>
-
 <p>Task ID: {taskId}</p>
 <p>Task Status: {taskStatus}</p>
 <p>Task Result: {JSON.stringify(taskResult)}</p>
 
-
-
-<!-- <form on:submit|preventDefault={test}>
-    <input type="submit" value="ASYNC">
-</form>
-
-<p>{data}</p>
-
-<script>
-    export let data = 'None';
-
-    async function test() {
-        const url = "http://127.0.0.1:5000/api/enqueue";
-        const requestData = { value: "someValue" }; // Include the necessary data
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(requestData) // Send the data as JSON
-        });
-
-        if (response.ok) {
-            const responseData = await response.json();
-            data = responseData.task_id; // Assuming the backend sends a task_id
-            console.log('Task ID:', data);
-        } else {
-            console.error('Request failed:', response.statusText);
-            data = 'Failed to get task ID';
-        }
-    }
-</script>
- -->
