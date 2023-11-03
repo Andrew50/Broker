d2cf928829bc26e40cc666005fab9852978b219c
billingsandrewjohnlaptop <billingsandrewjohn@gmail.com>
2023-11-02 22:34:18 -0400

    d

--- backend/app.py
+++ backend/app.py
@@ -11,36 +11,38 @@ class App:
         CORS(self.app)
         self.pool = Pool()
         self.tasks = {}
+        self.cores = 6
+        
+        
+
 
     def start_task(self, script_name):
 
 ##--------LINK SCRIPTS------------
         if script_name == 'get':
             func = Chart.get
-        if script_name =='match':
+            cores = 1
+        elif script_name =='match':
             func = test_func.god
-
+            cores = 3
+        else:
+            return jsonify({'task_id': 'failed'})
         data = request.json
         task_id = str(uuid.uuid4())
         def callback(result):
-            print(result.get())
-            self.tasks[task_id] = {'status': 'done', 'result': result.get()}
+            self.tasks[task_id] = {'status': 'done', 'result': result}
+        #for _ in range(cores):
         task = self.pool.apply_async(func, args=data.get('args', []), kwds=data.get('kwargs', {}), callback=callback)
         self.tasks[task_id] = {'status': 'pending', 'result': task}
         return jsonify({'task_id': task_id})
 
     def get_status(self, task_id):
         task_info = self.tasks.get(task_id)
-        if task_info is None:
-            return jsonify({'status': 'not found'}), 404
-
-       # elif task_info['status'] == 'done':# and task_info['result'].ready():
-          #  #result = task_info['result'].get()
-           # result = task_info['result']
-           # task_info.update({'status': 'done', 'result': result})
-        #3else:
-        print(task_info)
-        return jsonify(task_info)
+        try: 
+            return jsonify(task_info)
+        except:
+            return jsonify('loading')
+            
 
     def run(self):
         @self.app.route('/api/<script_name>', methods=['POST'])
--- backend/scripts/Chart.py
+++ backend/scripts/Chart.py
@@ -1,10 +1,14 @@
+import json
+
 try:
     from Data import Data, Database
 except:
     from .Data import Data, Database
-def get(ticker,tf,dt):
+def get():
     conn = Database()
-    return Data(conn,ticker).df
+    df = Data(conn,'AAPL').df
+    list_of_lists = df.tolist()
+    return json.dumps(list_of_lists)
 
 if __name__ == '__main__':
     print(get('AMZN','d',None))
