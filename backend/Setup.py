656b0edaa94554a59843bffa525ed1ba88a3536a
cs.benliu <cs.benliu@gmail.com>
2023-11-04 11:12:35 -0400

    234

--- backend/Setup.py
+++ backend/Setup.py
@@ -1,1 +1,8 @@
-
+from setuptools import setup
+from Cython.Build import cythonize
+import numpy
+# Use python backend/setup.py build_ext --inplace from C:/dev/Broker
+setup(
+    ext_modules = cythonize("backend/Odtw.pyx"),
+    include_dirs=[numpy.get_include()]
+)
