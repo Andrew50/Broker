3d99ce0dfebc7df4f5c6550d31691ecbf6576c1d
billingsandrewjohnlaptop <billingsandrewjohn@gmail.com>
2023-11-12 08:07:16 -0500

    d

--- frontend/src/routes/return/+page.svelte
+++ frontend/src/routes/return/+page.svelte
@@ -1,10 +1,6 @@
 <script>
     import { writable } from 'svelte/store';
 
-
-
-    
-
     let data1 = writable()
     let data1val;
     data1.subscribe((value) => {
@@ -12,9 +8,15 @@
     });
 
     async function startTask(bind_variable,func) {
+        event.preventDefault(); // Prevent the default form submission
+        const formData = new FormData(event.target);
+        let formValues = Array.from(formData.values()).join('-');
+        let args = `${func}-${formValues}`;
+
+        const url = `http://localhost:5057/fetch/${args}`;
         try{
-    
-        const response = await fetch(`http://localhost:5057/${func}`, {method: 'POST',headers: {'Content-Type': 'application/json'},});
+        console.log('request sent',url)
+        const response = await fetch(url, {method: 'POST',headers: {'Content-Type': 'application/json'},});
         if (!response.ok) {throw new Error('POST response not ok')}
         const responseData = await response.json();
         const task_id = responseData.task_id;
@@ -25,6 +27,7 @@
                 if (responseData.status === 'done') {
                     clearInterval(intervalId);
                     bind_variable.set(responseData.result);
+                    console.log(responseData.result);
                 }
         };
         const intervalId = setInterval(checkStatus, 500); // Check every 2 seconds
@@ -37,7 +40,17 @@
 <style>
 </style>
 <div>
-<form on:submit={(event) => startTask(data1,'groups/group1')}>
+<form on:submit={(event) => startTask(data1,'Chart_get')}>
+<div>
+        <input type="text" name="ticker" placeholder="Ticker" required>
+        </div>
+        <div>
+        <input type="text" name="datetime" placeholder="Datetime" required>
+        </div>
+        <div>
+        <input type="text" name="timeframe" placeholder="Timeframe" required>
+        </div>
+        
      <input type="submit" value="FETCH">
 </form> 
   <p>Result: {data1val}</p>
