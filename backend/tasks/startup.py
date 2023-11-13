6487ca35aef3571683d67f68180001c399fa4b9b
billingsandrewjohnlaptop <billingsandrewjohn@gmail.com>
2023-11-12 23:49:47 -0500

    d

--- backend/tasks/startup.py
+++
@@ -1,9 +0,0 @@
-from Data import Database, Dataset, Cache
-
-db = Database()
-ds = Dataset(db,'full').dfs
-
-cache_startup = [[ds,'ds']]
-
-cache = Cache()
-[ cache.set(data,key) for data, key in cache_startup]
