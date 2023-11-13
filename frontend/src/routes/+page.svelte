f74aa23581a0068bda64e54365c41514252b4417
billingsandrewjohnlaptop <billingsandrewjohn@gmail.com>
2023-11-12 22:47:46 -0500

    d

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -13,20 +13,37 @@
      match_data = value
      });
     
+
+
     let screener_data_store = writable()
     let screener_data;
     screener_data_store.subscribe((value) => {
     screener_data = value
     });
 
+
+
+
     export let chart_data_store = writable([
         {time: '2018-10-19', open: 180.34, high: 180.99, low: 178.57, close: 179.85}
     ]);
+
+
     export let chart_data;
+
+
+
     console.log(chart_data)
+
     chart_data_store.subscribe((value) => {
+
+
     try{chart_data = JSON.parse(value)}
     catch{chart_data = value}
+
+
+
+
         console.log(chart_data)
 
     });
