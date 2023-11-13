100732adff0a2c5f51c213729a46fa1c39bbdd86
billingsandrewjohnlaptop <billingsandrewjohn@gmail.com>
2023-11-13 15:27:57 -0500

    d

---
+++ backend.bat
@@ -0,0 +1,3 @@
+cd C:/dev/Broker/backend
+docker-compose up --build --scale myproj_worker=3
+pause
--- backend/start.sh
+++
@@ -1,5 +0,0 @@
-# Run your Python startup script
-python setup/startup.py
-
-# Then start Uvicorn server
-exec uvicorn api:app --host 0.0.0.0 --port 5057 --reload
