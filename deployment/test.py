85afd4a82162892d41cd6b011660fcc07cdf38b4
billingsandrewjohnlaptop <billingsandrewjohn@gmail.com>
2024-01-22 13:13:40 -0500

    Fix bugs and optimize code

---
+++ deployment/test.py
@@ -0,0 +1,23 @@
+#test all services
+
+#start backend docker
+
+#frontend
+#start frontend dev
+#use selenium to run trhough website functionality
+#create account
+#login
+#chart
+#match
+#train
+#screen
+
+
+
+#god idk if its possible because
+# - have to run in docker
+# - have to have data to test anything
+# - need sample to test models
+
+#could pull like 10 tickers
+#
