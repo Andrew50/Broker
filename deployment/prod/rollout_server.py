4c85b8d50e9e1c16b1fc43ac424849b3d1c45675
billingsandrewjohnlaptop <billingsandrewjohn@gmail.com>
2024-01-22 10:12:08 -0500

    Update deployment workflow and rollout server

--- deployment/prod/rollout_server.py
+++ deployment/prod/rollout_server.py
@@ -1,25 +1,23 @@
 from flask import Flask, request, jsonify
 import subprocess
 import os
+import traceback
 
 app = Flask(__name__)
-#f
+
 @app.route('/rollout', methods=['POST'])
 def rollout():
     try:
         print("Rollout request received", flush=True)
-        #os.chdir('/home/aj/dev/Broker')
-        #subprocess.run(['git', 'pull','origin','prod-beta'], capture_output=True, text=True)
         os.chdir('/home/aj/dev/Broker/deployment/prod')
         result = subprocess.run(['bash', 'rollout'], capture_output=True, text=True)
         if result.returncode == 0:
             return jsonify({"status": "success", "output": result.stdout}), 200
         else:
             raise Exception(result.stderr)
-            #return jsonify({"status": "error", "output": result.stderr}), 500
     except Exception as e:
         print(str(e), flush=True)
-        return jsonify({"status": "error", "message": str(e)}), 500
+        return jsonify({"status": "error", "message": str(e), "traceback": traceback.format_exc()}), 500
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
