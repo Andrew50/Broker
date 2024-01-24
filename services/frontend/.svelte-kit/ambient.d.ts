a3d551b90a818d6b5e59ce499d73c80c30d57e9b
billingsandrewjohnlaptop <billingsandrewjohn@gmail.com>
2024-01-24 18:43:54 -0500

    Update environment variables and version hash

--- services/frontend/.svelte-kit/ambient.d.ts
+++ services/frontend/.svelte-kit/ambient.d.ts
@@ -35,7 +35,7 @@ declare module '$env/static/private' {
 	export const ComSpec: string;
 	export const DriverData: string;
 	export const EDITOR: string;
-	export const EFC_4932: string;
+	export const EFC_10200: string;
 	export const FPS_BROWSER_APP_PROFILE_STRING: string;
 	export const FPS_BROWSER_USER_PROFILE_STRING: string;
 	export const GOPATH: string;
@@ -135,7 +135,7 @@ declare module '$env/dynamic/private' {
 		ComSpec: string;
 		DriverData: string;
 		EDITOR: string;
-		EFC_4932: string;
+		EFC_10200: string;
 		FPS_BROWSER_APP_PROFILE_STRING: string;
 		FPS_BROWSER_USER_PROFILE_STRING: string;
 		GOPATH: string;
